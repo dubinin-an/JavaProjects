c3b72eec64eac31750dc9051d287781a8f1ec023
dubinin-an <dan_1@mail.ru>
2017-01-09 13:41:13 +0300

    test

--- ru.dron2004.CollectionsTest/src/ru/dron2004/hashmaptest/HashMapTest.java
+++ ru.dron2004.CollectionsTest/src/ru/dron2004/hashmaptest/HashMapTest.java
@@ -4,6 +4,7 @@ public class HashMapTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, I'm just HashMap Test Project");
+		System.out.println("skdjghfreldkvgjflsdjkvl;s");
 	}
 
 }
